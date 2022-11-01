a5ed7ec1db7c521da8366446178cf6f90a869173
Verma Ritu <ritu.verma2@soprasteria.com>
2022-11-01 15:58:54 +0530

    Add existing project files to Git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularSwitch';
+  // title = 'AngularSwitch';
+  num : number = 0;
+  // title : string = 'ngIf Example';
+  // showMe! : boolean;
 }
